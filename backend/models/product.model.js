2b5f0e70b1c019da3f9be9f5c3916c1e59269cc3
Ruchelle <rbaybayan2@myseneca.ca>
2024-12-18 14:06:55 -0500

    mongodb and mongoose init setup done

---
+++ backend/models/product.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'; 
+
+// create Schema 
+const productSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    }, 
+    price: {
+        type: Number, 
+        required: true
+    }, 
+    image:{
+        type: String, 
+        required: true
+    },
+}, {
+    timestamps: true // whenever a product is created, timestamp ensure product has createdAt and updatedAt fields
+}); 
+
+// create product model, note depending on schema
+const Product = mongoose.model('Product', productSchema); 
+// mongoose will convert Product to lowercase and with s e.g. products 
+export default Product; 
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,27 @@
 // entry point for api
-import express from 'express'; 
+import express from 'express';  
+import dotenv from "dotenv"; 
+import { connectDB } from './config/db.js';
 
+dotenv.config();
 const app = express(); 
+
+app.post('/products', async (req, res) => {
+    const product = req.body; // user will send this data
+    
+}); 
 
 // to listen for connections on the specified host and port
 app.listen(5000, () => {
+    connectDB(); 
     console.log('Server started at http://localhost:5000'); 
 }); 
 
-// to route GET reqs to the specified path 
+// to route GET requests to the specified path 
+app.get('/products', (req, res) => {
+    res.send('Server is ready1234'); 
+});
+
+//console.log(process.env.MONGO_URI); 
+// ruchellebay
+// Hk6MvEkFVALc1VZL
